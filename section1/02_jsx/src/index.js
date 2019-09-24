daedf613b57e6876ac2caebc1d66e9485b78528d
Paulo Santos <paulo_sss@hotmail.com>
2019-09-24 19:38:10 +0100

    jsx app

---
+++ section1/02_jsx/src/index.js
@@ -0,0 +1,11 @@
+// Import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Create a React Component
+const App = () => {
+  return <div>Hello There!!</div>;
+};
+
+// Take the React component and show it on the screen
+ReactDOM.render(<App />, document.getElementById("root"));
