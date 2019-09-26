2f8a4cb9746d76db6a9525ae3e793d1dd5b19b0e
Paulo Santos <paulo.santos@kentpharm.co.uk>
2019-09-26 09:11:10 +0100

    jsx

--- section1/02_jsx/src/index.js
+++ section1/02_jsx/src/index.js
@@ -1,19 +1,21 @@
 // Import the React and ReactDOM libraries
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 // Create a React Component
 const App = () => {
+  const buttonText = { text: 'Click Me' };
+  const labelText = 'Enter Name:';
   return (
     <div>
-      <label htmlFor="name">Enter Name:</label>
+      <label htmlFor="name">{labelText}</label>
       <input type="text" name="name" id="name" />
-      <button type="submit" style={{ backgroundColor: "blue", color: "white" }}>
-        Submit
+      <button type="submit" style={{ backgroundColor: 'blue', color: 'white' }}>
+        {buttonText.text}
       </button>
     </div>
   );
 };
 
 // Take the React component and show it on the screen
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
