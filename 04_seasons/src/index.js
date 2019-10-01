a2d1c150a5ed01d58bac2fb5b6520f0d720d6a71
Paulo Santos <paulo_sss@hotmail.com>
2019-10-01 18:49:05 +0100

    refactor to use componentDidMount

--- 04_seasons/src/index.js
+++ 04_seasons/src/index.js
@@ -2,26 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = { lat: null, errMessage: ''}
-
-        window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({lat: position.coords.latitude})
-            },
-            err => {this.setState({errMessage: err.message})}
-        )
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, errMessage: "" };
+  }
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errMessage: err.message })
+    );
+  }
+  render() {
+    if (!this.state.lat && this.state.errMessage) {
+      return <div>Error: {this.state.errMessage}</div>;
     }
-    render() {
-        if (!this.state.lat && this.state.errMessage) {
-            return <div>Error: {this.state.errMessage}</div>
-        }
-        if (this.state.lat && !this.state.errMessage) {
-            return <div>Latitude: {this.state.lat}</div>
-        }
-        return <div>Loading...</div>
+    if (this.state.lat && !this.state.errMessage) {
+      return <div>Latitude: {this.state.lat}</div>;
     }
+    return <div>Loading...</div>;
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
