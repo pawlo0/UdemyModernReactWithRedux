4c0e808e52e10697ce6e5c238e4d19d8e34a83da
Paulo Santos <paulo_sss@hotmail.com>
2019-10-01 15:19:29 +0000

    handle error gracefully

--- 04_seasons/src/index.js
+++ 04_seasons/src/index.js
@@ -4,17 +4,23 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
     constructor(props){
         super(props)
-        this.state = { lat: null}
+        this.state = { lat: null, errMessage: ''}
 
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 this.setState({lat: position.coords.latitude})
             },
-            err => console.log(err)
+            err => {this.setState({errMessage: err.message})}
         )
     }
     render() {
-        return <div>Latitude: {this.state.lat}</div>
+        if (!this.state.lat && this.state.errMessage) {
+            return <div>Error: {this.state.errMessage}</div>
+        }
+        if (this.state.lat && !this.state.errMessage) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+        return <div>Loading...</div>
     }
 }
 
