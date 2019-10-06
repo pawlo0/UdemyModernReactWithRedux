8b6543ed120d242513dc587f91ca82d36d15e8df
Paulo Santos <paulo_sss@hotmail.com>
2019-10-06 17:57:12 +0100

    adds structure to the project

--- 05_pics/src/index.js
+++ 05_pics/src/index.js
@@ -1,14 +1,5 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>hello world!!</h1>
-      </div>
-    );
-  }
-}
+import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
