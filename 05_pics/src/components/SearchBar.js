0e440788e4ebdba45f6912ae70a452d2e5af3d19
Paulo Santos <paulo_sss@hotmail.com>
2019-10-06 18:57:55 +0100

    adds event handler

--- 05_pics/src/components/SearchBar.js
+++ 05_pics/src/components/SearchBar.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 
 export class SearchBar extends Component {
+  state = { term: "" };
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" name="search" id="search" />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
