07f525c07d60d6344048516df02e18133dcbb951
Paulo Santos <paulo_sss@hotmail.com>
2019-10-07 18:35:30 +0000

    communicating child to parent by invoking callbacks in children

--- 05_pics/src/components/SearchBar.js
+++ 05_pics/src/components/SearchBar.js
@@ -1,29 +1,29 @@
 import React, { Component } from "react";
 
 export class SearchBar extends Component {
-  state = { term: "" };
+    state = { term: "" };
 
-  onFormSubmit = e => {
-    e.preventDefault();
-    console.log(this.state.term);
-  };
+    onFormSubmit = e => {
+        e.preventDefault();
+        this.props.onSubmit(this.state.term);
+    };
 
-  render() {
-    return (
-      <div className="ui segment">
-        <form className="ui form" onSubmit={this.onFormSubmit}>
-          <div className="field">
-            <label>Image Search</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={e => this.setState({ term: e.target.value })}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="ui segment">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={e => this.setState({ term: e.target.value })}
+                        />
+                    </div>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default SearchBar;
