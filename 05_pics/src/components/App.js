b75430a63d20fe2ee1c7b8cd8429bab119a0d8d5
Paulo Santos <paulo_sss@hotmail.com>
2019-10-07 18:54:14 +0000

    imports axios and starts fetching from unsplash

--- 05_pics/src/components/App.js
+++ 05_pics/src/components/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import axios from 'axios';
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
     onSearchSubmit(term) {
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: ' Client-ID 61d8c024cd74b22653ea35ce524fc0cf14cec24050d88894e1924b97a7d3d5ba'
+            }
+        })
         console.log(term)
     }
 
