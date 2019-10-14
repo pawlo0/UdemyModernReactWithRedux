c2f9006eb09b704bc852ace93e6641380dd0bd49
Paulo Santos <paulo_sss@hotmail.com>
2019-10-14 18:15:59 +0000

    handles onSubmit

--- 06_videos/src/components/App.js
+++ 06_videos/src/components/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from '../apis/youtube';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar />
-      </div>
-    );
-  }
+
+    state = { videos: [] };
+
+    onTermSubmit = async searchTerm => {
+        const response = await youtube.get('/search', {
+            params: {
+                q: searchTerm
+            }
+        });
+
+        this.setState({ videos: response.data.items });
+    }
+
+    render() {
+        return (
+            <div className="ui container">
+                <SearchBar onTermSubmit={this.onTermSubmit} />
+                I have {this.state.videos.length} videos.
+            </div>
+        );
+    }
 }
--- 06_videos/src/components/SearchBar.js
+++ 06_videos/src/components/SearchBar.js
@@ -1,31 +1,31 @@
 import React, { Component } from "react";
 
 export default class SearchBar extends Component {
-  state = { searchTerm: "" };
+    state = { searchTerm: "" };
 
-  onInputChange = e => {
-    this.setState({ searchTerm: e.target.value });
-  };
+    onInputChange = e => {
+        this.setState({ searchTerm: e.target.value });
+    };
 
-  onFormSubmit = e => {
-    e.preventDefault();
-    //this.props.onSubmit(this.state.searchTerm);
-  };
+    onFormSubmit = e => {
+        e.preventDefault();
+        this.props.onTermSubmit(this.state.searchTerm);
+    };
 
-  render() {
-    return (
-      <div className="search-bar ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
-          <div className="field">
-            <label>Video Search</label>
-            <input
-              type="text"
-              onChange={this.onInputChange}
-              value={this.state.searchTerm}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="search-bar ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input
+                            type="text"
+                            onChange={this.onInputChange}
+                            value={this.state.searchTerm}
+                        />
+                    </div>
+                </form>
+            </div>
+        );
+    }
 }
