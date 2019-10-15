20aaa783e198a1877e295f806c14b2bda04490f8
Paulo Santos <paulo_sss@hotmail.com>
2019-10-15 17:42:28 +0000

    adds callback to select video

--- 06_videos/src/components/App.js
+++ 06_videos/src/components/App.js
@@ -1,28 +1,35 @@
 import React from "react";
 import SearchBar from "./SearchBar";
-import youtube from '../apis/youtube';
-import VideoList from './VideoList';
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
 
 export default class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
 
-    state = { videos: [] };
+  onTermSubmit = async searchTerm => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: searchTerm
+      }
+    });
 
-    onTermSubmit = async searchTerm => {
-        const response = await youtube.get('/search', {
-            params: {
-                q: searchTerm
-            }
-        });
+    this.setState({ videos: response.data.items });
+  };
 
-        this.setState({ videos: response.data.items });
-    }
+  onVideoSelect = video => {
+    this.setState({ selectedVideo: video });
+    console.log("From the App ", video);
+  };
 
-    render() {
-        return (
-            <div className="ui container">
-                <SearchBar onTermSubmit={this.onTermSubmit} />
-                <VideoList videos={this.state.videos} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onTermSubmit={this.onTermSubmit} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={this.onVideoSelect}
+        />
+      </div>
+    );
+  }
 }
