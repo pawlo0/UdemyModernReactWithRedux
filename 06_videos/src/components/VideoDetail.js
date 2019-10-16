7ac770e768d16b408a241f3513c4080e248ce1e4
Paulo Santos <paulo_sss@hotmail.com>
2019-10-16 19:18:40 +0100

    more styling

--- 06_videos/src/components/VideoDetail.js
+++ 06_videos/src/components/VideoDetail.js
@@ -7,7 +7,10 @@ const VideoDetail = ({ video }) => {
   return (
     <div>
       <div className="ui embed">
-        <iframe src={`https://www.youtube.com/embed/${video.id.videoId}`} />
+        <iframe
+          src={`https://www.youtube.com/embed/${video.id.videoId}`}
+          title="video player"
+        />
       </div>
       <div className="ui segment">
         <h4 className="header">{video.snippet.title}</h4>
