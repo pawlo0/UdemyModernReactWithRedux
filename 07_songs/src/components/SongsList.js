ec1e7c82af2333ea3932ea645bf6601ecc570697
Paulo Santos <paulo_sss@hotmail.com>
2019-10-19 18:44:15 +0100

    show list songs

--- 07_songs/src/components/SongsList.js
+++ 07_songs/src/components/SongsList.js
@@ -2,8 +2,21 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 export class SongsList extends Component {
+  renderList() {
+    return this.props.songs.map(song => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary">Select</button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return <div>Songs List</div>;
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
