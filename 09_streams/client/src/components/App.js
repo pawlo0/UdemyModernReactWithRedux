bade5c039d858bd88c03922d77ee6e80417df581
Paulo Santos <paulo_sss@hotmail.com>
2019-11-01 22:20:04 +0000

    wire up routes

--- 09_streams/client/src/components/App.js
+++ 09_streams/client/src/components/App.js
@@ -1,27 +1,19 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
-
-const PageOne = () => (
-  <div>
-    Page One
-    <br />
-    <Link to="/pagetwo">Navigate to page Two</Link>
-  </div>
-);
-
-const PageTwo = () => (
-  <div>
-    Page Two
-    <br />
-    <Link to="/">Navigate to page One</Link>
-  </div>
-);
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => (
   <div>
     <BrowserRouter>
-      <Route path="/" exact component={PageOne} />
-      <Route path="/pagetwo" component={PageTwo} />
+      <Route path="/" exact component={StreamList} />
+      <Route path="/streams/new" component={StreamCreate} />
+      <Route path="/streams/edit" component={StreamEdit} />
+      <Route path="/streams/delete" component={StreamDelete} />
+      <Route path="/streams/show" component={StreamShow} />
     </BrowserRouter>
   </div>
 );
