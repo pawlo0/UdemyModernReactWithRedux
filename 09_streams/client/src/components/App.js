54c27bd28fdae10b48eb90706aab89f3d7f99fd8
Paulo Santos <paulo_sss@hotmail.com>
2019-11-01 16:52:07 +0000

    adds links

--- 09_streams/client/src/components/App.js
+++ 09_streams/client/src/components/App.js
@@ -1,9 +1,21 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
-const PageOne = () => <div>Page One</div>;
+const PageOne = () => (
+  <div>
+    Page One
+    <br />
+    <Link to="/pagetwo">Navigate to page Two</Link>
+  </div>
+);
 
-const PageTwo = () => <div>Page Two</div>;
+const PageTwo = () => (
+  <div>
+    Page Two
+    <br />
+    <Link to="/">Navigate to page One</Link>
+  </div>
+);
 
 const App = () => (
   <div>
