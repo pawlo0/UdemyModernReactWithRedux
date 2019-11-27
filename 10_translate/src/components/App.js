1f9bb1d058620b9b84d03720558c8af5f9397c16
Paulo Santos <paulo_sss@hotmail.com>
2019-11-27 19:01:21 +0000

    initiates state

--- 10_translate/src/components/App.js
+++ 10_translate/src/components/App.js
@@ -1,7 +1,26 @@
 import React, { Component } from "react";
 
 export default class App extends Component {
+  state = { language: "english" };
+
+  onLanguageChange = language => this.setState({ language });
+
   render() {
-    return <div className="ui container">App</div>;
+    return (
+      <div className="ui container">
+        <div>
+          Select Languange:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange("english")}
+          />
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange("dutch")}
+          />
+        </div>
+        {this.state.language}
+      </div>
+    );
   }
 }
