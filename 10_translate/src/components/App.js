ab2c0eb00cd3e1a0037f9d7fc14aa06a8821d7f6
Paulo Santos <paulo_sss@hotmail.com>
2019-11-30 20:00:55 +0000

    context provider and consumer

--- 10_translate/src/components/App.js
+++ 10_translate/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import UserCreate from "./UserCreate";
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 export default class App extends Component {
   state = { language: "english" };
@@ -20,7 +22,11 @@ export default class App extends Component {
             onClick={() => this.onLanguageChange("dutch")}
           />
         </div>
-        <UserCreate />
+        <ColorContext.Provider value="red">
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
       </div>
     );
   }
--- 10_translate/src/components/Button.js
+++ 10_translate/src/components/Button.js
@@ -1,7 +1,22 @@
 import React, { Component } from "react";
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 export default class Button extends Component {
+  renderButton(color) {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {value => (value === "english" ? "Submit" : "Voorleggen")}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
   render() {
-    return <button className="ui button primary">Submit</button>;
+    return (
+      <ColorContext.Consumer>
+        {color => this.renderButton(color)}
+      </ColorContext.Consumer>
+    );
   }
 }
