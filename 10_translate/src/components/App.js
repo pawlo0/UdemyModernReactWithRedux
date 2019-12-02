6e846091432d7c4d8c997f2ae3575acfed7878f4
Paulo Santos <paulo_sss@hotmail.com>
2019-12-02 16:54:15 +0000

    extracted onLanguageChange to new component

--- 10_translate/src/components/App.js
+++ 10_translate/src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import UserCreate from "./UserCreate";
 import LanguageContext from "../contexts/LanguageContext";
 import ColorContext from "../contexts/ColorContext";
+import LanguageSelector from "./LanguageSelector";
 
 export default class App extends Component {
   state = { language: "english" };
@@ -11,17 +12,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="ui container">
-        <div>
-          Select Languange:
-          <i
-            className="flag us"
-            onClick={() => this.onLanguageChange("english")}
-          />
-          <i
-            className="flag nl"
-            onClick={() => this.onLanguageChange("dutch")}
-          />
-        </div>
+        <LanguageSelector onLanguageChange={this.onLanguageChange} />
         <ColorContext.Provider value="red">
           <LanguageContext.Provider value={this.state.language}>
             <UserCreate />
